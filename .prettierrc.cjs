6f3c64b61146bf099414bce4899851741a52d242
Kenji Suzuki <41730006+suzulabo@users.noreply.github.com>
2022-09-25 12:21:19 +0900

    Use .prettierrc.cjs

---
+++ .prettierrc.cjs
@@ -0,0 +1,10 @@
+/** @type {import('prettier').Config} */
+const config = {
+  printWidth: 80,
+  tabWidth: 2,
+  quoteProps: 'consistent',
+  singleQuote: true,
+  jsxSingleQuote: false,
+};
+
+module.exports = config;
