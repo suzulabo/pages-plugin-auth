b9f467780f907ece6e61b8910b6ec11beeee97bc
Kenji Suzuki <41730006+suzulabo@users.noreply.github.com>
2022-11-06 03:09:30 +0900

    use @suzulabo/code-config-typescript

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,56 +1,7 @@
 /** @type {import('eslint').Linter.Config} */
 const config = {
   root: true,
-  extends: ['eslint:recommended', 'prettier'],
-  overrides: [
-    {
-      files: ['*.ts', '*.tsx'],
-      parser: '@typescript-eslint/parser',
-      parserOptions: {
-        project: 'tsconfig.json',
-      },
-      plugins: ['@typescript-eslint', 'import'],
-      extends: [
-        'eslint:recommended',
-        'plugin:@typescript-eslint/recommended',
-        'plugin:import/recommended',
-        'plugin:import/typescript',
-        'prettier',
-      ],
-      rules: {
-        '@typescript-eslint/no-floating-promises': 'error',
-        '@typescript-eslint/no-misused-promises': 'error',
-        'require-await': 'off',
-        '@typescript-eslint/require-await': 'warn',
-
-        'import/order': [
-          'warn',
-          {
-            'groups': [
-              'builtin',
-              'external',
-              ['internal', 'parent', 'sibling', 'index'],
-              'object',
-              'type',
-            ],
-            'newlines-between': 'always',
-            'alphabetize': {
-              order: 'asc',
-              caseInsensitive: false,
-            },
-            'warnOnUnassignedImports': true,
-          },
-        ],
-
-        'import/no-extraneous-dependencies': 'error',
-      },
-      settings: {
-        'import/resolver': {
-          typescript: true,
-        },
-      },
-    },
-  ],
+  ...require('@suzulabo/code-config-typescript/eslint'),
 };
 
 module.exports = config;
