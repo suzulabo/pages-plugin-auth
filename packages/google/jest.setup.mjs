ac9bb78ed59cab68342eb71af1295532d10c3222
Kenji Suzuki <41730006+suzulabo@users.noreply.github.com>
2022-11-05 12:26:17 +0900

    delete jest files

--- packages/google/jest.setup.mjs
+++
@@ -1,8 +0,0 @@
-import { webcrypto } from 'crypto';
-
-import { Request, Headers } from 'node-fetch';
-
-/* global global */
-global.Request = Request;
-global.Headers = Headers;
-global.crypto = webcrypto;
