ae2298b355d3d009c8e7de764447c0015efa3fd9
Kenji Suzuki <41730006+suzulabo@users.noreply.github.com>
2022-11-05 23:05:20 +0900

    typescript config move to overrides

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,50 +1,56 @@
 /** @type {import('eslint').Linter.Config} */
 const config = {
   root: true,
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    project: 'tsconfig.json',
-  },
-  plugins: ['@typescript-eslint', 'import'],
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:import/recommended',
-    'plugin:import/typescript',
-    'prettier',
-  ],
-  rules: {
-    '@typescript-eslint/no-floating-promises': 'error',
-    '@typescript-eslint/no-misused-promises': 'error',
-    'require-await': 'off',
-    '@typescript-eslint/require-await': 'warn',
-
-    'import/order': [
-      'warn',
-      {
-        'groups': [
-          'builtin',
-          'external',
-          ['internal', 'parent', 'sibling', 'index'],
-          'object',
-          'type',
-        ],
-        'newlines-between': 'always',
-        'alphabetize': {
-          order: 'asc',
-          caseInsensitive: false,
-        },
-        'warnOnUnassignedImports': true,
+  extends: ['eslint:recommended', 'prettier'],
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx'],
+      parser: '@typescript-eslint/parser',
+      parserOptions: {
+        project: 'tsconfig.json',
       },
-    ],
+      plugins: ['@typescript-eslint', 'import'],
+      extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:import/recommended',
+        'plugin:import/typescript',
+        'prettier',
+      ],
+      rules: {
+        '@typescript-eslint/no-floating-promises': 'error',
+        '@typescript-eslint/no-misused-promises': 'error',
+        'require-await': 'off',
+        '@typescript-eslint/require-await': 'warn',
 
-    'import/no-extraneous-dependencies': 'error',
-  },
-  settings: {
-    'import/resolver': {
-      typescript: true,
+        'import/order': [
+          'warn',
+          {
+            'groups': [
+              'builtin',
+              'external',
+              ['internal', 'parent', 'sibling', 'index'],
+              'object',
+              'type',
+            ],
+            'newlines-between': 'always',
+            'alphabetize': {
+              order: 'asc',
+              caseInsensitive: false,
+            },
+            'warnOnUnassignedImports': true,
+          },
+        ],
+
+        'import/no-extraneous-dependencies': 'error',
+      },
+      settings: {
+        'import/resolver': {
+          typescript: true,
+        },
+      },
     },
-  },
+  ],
 };
 
 module.exports = config;
